2742292b653ebcc65c3d455504b0656cbab89bc9
pixelblob <phillipsfamily.ethan@gmail.com>
2023-08-25 23:40:11 +0200

    aaaaaa

---
+++ test.pas
@@ -0,0 +1,24 @@
+unit test;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  Tplayer = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  player: Tplayer;
+
+implementation
+
+{$R *.dfm}
+
+end.
